6ad31a79d955e42a70dd3b945a40cc72ae78bc1f
mpbmpb <markkeyster@gmail.com>
2022-04-02 00:47:07 +0200

    Added threadsafe caching class with locks
    Added 2 readerwriterlockslim fields for both dictionary and queue
    Used each lock where appropriate
    
    Copied tests from memorycache class and applied them to threadsafecache class
    
    ready to start implementing GetOrSet method

---
+++ src/simplecache/ThreadSafeCache.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Concurrent;
+
+namespace simplecache;
+
+public class ThreadSafeCache<T>
+{
+    private readonly ConcurrentDictionary<object, T> _dictionary;
+    private ConcurrentQueue<object> _keyQueue { get; set; } = new();
+    private CacheOptions _options { get; init; } = new();
+    private readonly ReaderWriterLockSlim _cacheLock = new();
+    private readonly ReaderWriterLockSlim _queueLock = new();
+
+    public ThreadSafeCache()
+    {
+        _dictionary = new();
+    }
+
+    public ThreadSafeCache(CacheOptions options)
+        : this()
+    {
+        _options = options;
+    }
+    
+    public bool Set(object key, T value)
+    {
+        if (_dictionary.Count >= _options.SizeLimit)
+        {
+            var pruned = TryPrune();
+            if (!pruned)
+                return false;
+        }
+
+        var success = false;
+        _cacheLock.EnterWriteLock();
+        try
+        {
+            success =_dictionary.TryAdd(key, value);
+            
+        }
+        finally
+        {
+            _cacheLock.ExitWriteLock();
+            if (success)
+            {
+                EnqueueKey(key);
+            }
+        }
+        
+        return success;
+    }
+
+    private void EnqueueKey(object key)
+    {
+        _queueLock.EnterWriteLock();
+        try
+        {
+            _keyQueue.Enqueue(key);
+        }
+        finally
+        {
+            _queueLock.ExitWriteLock();
+        }
+    }
+
+    public bool TryGet(object key, out T value)
+    {
+        var success = false;
+        _cacheLock.EnterReadLock();
+        try
+        {
+            success = _dictionary.TryGetValue(key, out value!);
+        }
+        finally
+        {
+            _cacheLock.ExitReadLock();
+            if (success && _options.EvictionPolicy == Evict.LeastRecentlyUsed)
+                RefreshQueue(key);
+        }
+        
+        return success;
+    }
+
+    private void RefreshQueue(object key)
+    {
+        _queueLock.EnterWriteLock();
+        try
+        {
+            _keyQueue = new ConcurrentQueue<object>(_keyQueue.Where(x => !x.Equals(key)));
+            _keyQueue.Enqueue(key);
+        }
+        finally
+        {
+            _queueLock.ExitWriteLock();
+        }
+    }
+
+    public bool TryPrune()
+    {
+        var success = false;
+        _cacheLock.EnterWriteLock();
+        _queueLock.EnterWriteLock();
+        try
+        {
+            _keyQueue.TryDequeue(out var key);
+            success = _dictionary.TryRemove(key!, out _);
+        }
+        finally
+        {
+            _cacheLock.ExitWriteLock();
+            _queueLock.ExitWriteLock();
+        }
+
+        return success;
+    }
+}
---
+++ test/simplecache.tests/unit/ThreadSafeCacheTests.cs
@@ -0,0 +1,89 @@
+﻿namespace simplecache.tests.unit;
+
+public class ThreadSafeCacheTests
+{
+    [Theory]
+    [InlineData(1, "test value")]
+    [InlineData(2, 42)]
+    [InlineData("3", typeof(TimeOnly))]
+    public void Add_ShouldAddObject_ToCache<T>(object key, T value)
+    {
+        var sut = new ThreadSafeCache<T>();
+
+        sut.Set(key, value);
+
+        var success = sut.TryGet(key, out var result);
+
+        success.Should().BeTrue();
+        result.Should().Be(value);
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(8)]
+    [InlineData(42)]
+    [InlineData(2132)]
+    public void Add_ShouldEvictOldestEntry_WhenSizeLimitIsReached(int sizeLimit)
+    {
+        var sut = new ThreadSafeCache<string>(new CacheOptions { SizeLimit = 2 });
+
+        for (int i = 0; i <= sizeLimit; i++)
+        {
+            sut.Set(i, $"test entry #{i}");
+        }
+
+        var oldestEntry = sut.TryGet(0, out _);
+
+        oldestEntry.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(8)]
+    [InlineData(42)]
+    [InlineData(2132)]
+    public void Add_ShouldEvict_Only_OldestEntry_WhenSizeLimitIsReached(int sizeLimit)
+    {
+        var sut = new ThreadSafeCache<string>(new CacheOptions { SizeLimit = sizeLimit });
+
+        for (int i = 0; i <= sizeLimit; i++)
+        {
+            sut.Set(i, $"test entry #{i}");
+        }
+
+        var oldestEntry = sut.TryGet(0, out _);
+        var secondOldest = sut.TryGet(1, out var result);
+
+        oldestEntry.Should().BeFalse();
+        secondOldest.Should().BeTrue();
+        result.Should().Match("test entry #1");
+    }
+
+    [Fact]
+    public void New_ThreadSafeCacheWithoutOptions_ShouldHave_NoSizeLimit()
+    {
+        var sut = new ThreadSafeCache<string>();
+        var result = sut.GetPrivateProperty<CacheOptions>("_options");
+        
+        result.SizeLimit.Should().BeNull();
+    }
+
+    [Fact]
+    public void Add_ShouldEvict_LeastRecentlyUsedEntry_WhenThisOptionIsSet()
+    {
+        var sut = new ThreadSafeCache<string>(new CacheOptions { SizeLimit = 2, EvictionPolicy = Evict.LeastRecentlyUsed });
+
+        sut.Set(1, "1");
+        sut.Set(2, "2");
+        sut.TryGet(1, out _);
+        sut.Set(3, "3");
+
+        var oldestEntry = sut.TryGet(1, out var result);
+        var leastRecentEntry = sut.TryGet(2, out _);
+
+        leastRecentEntry.Should().BeFalse();
+        oldestEntry.Should().BeTrue();
+        result.Should().Match("1");
+    }
+
+}
